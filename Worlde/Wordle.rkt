f038ee9de7533defb00488d61485273f89cd0f6a
Brian Daniels <57424473+IAmBdan@users.noreply.github.com>
2023-09-21 11:00:39 -0700

    Update and rename Wordle.rkt to Worlde/Wordle.rkt

--- Worlde/Wordle.rkt
+++ Worlde/Wordle.rkt
@@ -1,39 +1,19 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
+
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname hw9-problem1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/batch-io)
 (require 2htdp/image)
 (require 2htdp/universe)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Problem 1
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; You are now ready to implement Wordle (https://www.nytimes.com/games/wordle/),
-; at least most of it ;)
-
-; You've actually done quite a bit this semester already, and so the first step
-; is to review some of the definitions you'll use to build your game.
-
-; TODO 0/8: Read the following sections of (annotated) definitions - as needed,
-;           go back to the sample solutions & walkthrough videos of the
-;           associated assignments in order to make sure you understand each
-;           piece of the program we have thus far!
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
 ; Data (General)
 ; --------------
 ;
-; - Pair (NEW)
+; - Pair 
 ;   A generalized definition for associating two arbitrary
 ;   data types; this is used both for LetterStatusPair and
 ;   in helping to visualize elements of the game by
 ;   pairing up a game object, such as a guesed letter, with
 ;   a function used to visualize that type of object
 ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define-struct pair [first second])
 
@@ -41,23 +21,22 @@
 ; Interpretation: a pairing of two values
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; Data (Letters)
 ; --------------
 ;
-; - LetterStatus (Homework 2)
+; - LetterStatus 
 ;   A categorization of scored letters
 ;
-; - LetterStatusPair (Homework 3)
+; - LetterStatusPair 
 ;   An association between a specific letter and its scored
 ;   status; updated to use Pair
 ;
-; - VizPair (NEW)
+; - VizPair
 ;   An association between a value and a function that can
 ;   produce an image for it
 ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ; A LetterStatus (LS) is one of:
 ; - "wrong"
@@ -76,10 +55,9 @@
 
 ; A [VizPair X] is a [Pair X [X -> Image]]
 ; Interpretation: a pairing of a value with a function that
-; produces a vizualization of it
+; produces a visualization of it
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; Constants (Visualization)
 ; -------------------------
@@ -94,7 +72,6 @@
 ;
 ; - Buffer space between game objects (GAP)
 ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define BG-COLOR "white")
 (define BORDER-COLOR "dimgray")
@@ -104,17 +81,15 @@
 
 (define GAP (square 5 "solid" BG-COLOR))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; Functions (General)
 ; -------------------
 ;
-; - mymap2 (Lab 9)
+; - mymap2 
 ;   Abstraction for capturing the result of applying a
 ;   function to the elements from two parallel lists
 ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ; mymap2 : (X Y Z) [List-of X] [List-of Y] [X Y -> Z] -> [List-of Z]
 ; produces a result list by applying the supplied function
@@ -152,27 +127,25 @@
            (mymap2 (rest l1) (rest l2) f))]))
   
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
 ; Functions (Visualizing Letters)
 ; -------------------------------
 ;
-; - boxed-letter (Homework 6)
+; - boxed-letter 
 ;   Abstraction for drawing a letter over a box; uses an
 ;   updated test to reflect that a "blank" box for guesses
 ;   is now local to a helper
 ;
-; - guess-letter->image (Homework 6)
+; - guess-letter->image 
 ;   Uses boxed-letter to provide a simple way to visualize
 ;   letters and empty spaces that have yet to be scored;
 ;   updated to make its background (blank) a local helper
 ;
-; - lsp->image (Homework 6, Lab 8)
+; - lsp->image 
 ;   Uses boxed-letter to provide a simple way to visualize
 ;   scored letters; updated to (a) make its background a
 ;   set of local functions and (b) utilize pair data
 ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ; boxed-letter : 1String NonNegReal [NonNegReal -> Image] -> Image
 ; produces an image of a letter on a background of a particular size
@@ -259,31 +232,28 @@
      (ls->color-fn (pair-second lsp)))))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
 ; Functions (Visualizing Letters)
 ; -------------------------------
 ;
-; - stack (Lab 8)
+; - stack 
 ;   Abstraction for separating a set of images using a
 ;   pairwise orientation function
 ;
-; - stack/v (Homework 7, Lab 8)
+; - stack/v 
 ;   Uses stack to easily visualize a list of images in
 ;   a horizontal fashion
 ; 
-; - stack/h (Homework 7, Lab 8)
+; - stack/h 
 ;   Uses stack to easily visualize a list of images in
 ;   a horizontal fashion
 ;
-; - row->image (NEW)
+; - row->image 
 ;   Uses stack/h to easily visualize a list of objects, each
 ;   with an associated visualization function (e.g., a list
 ;   of letters with one of the functions from the preceding
 ;   section).
 ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 
 ; stack : [List-of Images] [Image Image -> Image] -> Image
 ; space separates a list of images via a pairwise orientation function
@@ -450,15 +420,6 @@
     ips)))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-; Now let's get to work :)
-
-
-; In Homework 6, you created the following very useful abstraction for
-; determining if a value is present in a list...
-
 
 ; value-in-list? : (X) X [List-of X] [X X -> Boolean] -> Boolean
 ; is the supplied value in the list according to the supplied predicate?
@@ -480,14 +441,6 @@
 
 
  
-; TODO 1/8: This function should remind you a great deal of one of the ISL list
-;           abstractions - so as a warm-up, use that abstraction to rewrite the
-;           code above; if you are successful, all the supplied tests will still
-;           pass (once uncommented) AND you will have a much better abstraction
-;           design to use when wordle'ing. ALSO, you can then make use of
-;           common special cases, like string-in-list? (also from Homework 6,
-;           supplied below).
-
 
 ; string-in-list? : String [List-of String] -> Boolean
 ; is the supplied string in the list? 
@@ -505,14 +458,6 @@
 (define (value-in-list? x lox p?) 
   (ormap (λ (e) (p? x e)) lox))
    
-
-; TODO 2/8: Now, finish designing the function read-dictionary, which reads and
-;           capitalizes all the words from a supplied file (assumed to be one
-;           word per line). The supplied tests are sufficient for your function
-;           and should both pass once complete (which means, please do NOT
-;           create a file named "BAD.EVIL" and make sure to have the provided
-;           "little.txt" file in the same folder as this code.
-
 
 ; read-dictionary : String -> [List-of String]
 ; reads the words in a supplied file and capitalizes them all
@@ -537,25 +482,6 @@
 (define (read-dictionary s) 
   (if (file-exists? s) (map string-upcase (read-lines s)) '())) 
 
-
-
-; TODO 3/8: Your next task is to finish designing the score function, which
-;           implements a simplified version of Wordle scoring, wherein each
-;           letter of the supplied guess is independently associated with a
-;           status in the supplied correct word.
-;
-;           Notes:
-;           - Make sure you read and understand all the supplied tests, which
-;             capture how the function is supposed to work. Once your code is
-;             complete, all of these tests should pass (and that is sufficient
-;             testing for this function).
-;           - You are highly encouraged to use mymap2 (see "Functions (General)"
-;             section above), as well as string-in-list? (TODO #1).
-;           - FYI: this approach is not 100% consistent with the NY Times, which
-;             handles repeated letters in a somewhat more complicated fashion
-;             (that we'll consider this in a later assignment!).
-
-
 ; score : String String -> [List-of LetterStatusPair]
 ; Given a guess and the correct string (assumed to be the same length, and both
 ; uppercase), produce the resulting pairing of each character and its status
@@ -571,7 +497,6 @@
     (mymap2 (explode l1) (explode l2) checker)))
            
              
-
 (check-expect (score "ABC" "ABC")
               (list (make-pair "A" LS-RIGHT)
                     (make-pair "B" LS-RIGHT)
@@ -671,23 +596,6 @@
 ; When a player is actively guessing, they may have fewer letters than are
 ; required of a full Wordle guess (5, in the NYT version).
 
-; TODO 4/8: Finish designing the function partial-guess->full-guess, which pads
-;           a string with spaces up to the supplied length.
-;
-;           Notes:
-;           - You have been supplied a sufficient set of tests for this
-;             function; they should all pass once your code is written.
-;           - You have ALSO been supplied a working coded version of the
-;             function that uses string-based helpers; to practice ISL list
-;             abstractions, your code is REQUIRED to instead solve this task
-;             using explode (to convert the string to a list of 1String's),
-;             build-list (to produce extra spaces, as necessary), append (to
-;             combine the guess with its padding), and then implode (to convert
-;             the list of 1String's back into a string).
-;           - Once these tests pass, uncomment the partial-guess->image design
-;             below, which allows you to easily visualize a partial guess:
-;             BOTH the current guess, as well as the remaining (blank) guesses.
-
 
 ; partial-guess->full-guess : String Nat -> String
 ; expands a guess with spaces to fill up the supplied length
@@ -704,9 +612,6 @@
    s
    (replicate (- len (string-length s)) " ")))
 
-; YOUR (LIST-BASED) partial-guess->full-guess CODE HERE!
-; (once this is working, uncomment & review the code below)
-
 (define (partial-guess->full-guess s n)
   (local [
           ; spaces-in-list : Nat -> [List-of Strings]
@@ -720,7 +625,6 @@
     (implode (add-to-list s))))
  
                       
-   
 ; partial-guess->image : String Nat -> Image
 ; visualizes a partial guess (up to a supplied length,
 ; assumed to be at least as long as the supplied string)
@@ -768,12 +672,6 @@
      (partial-guess->full-guess s len)))))
 
 
-
-
-; Now you are ready to implement your program :)
-; First, review the following representation we'll use for state...
-
-
 (define-struct wordle [past current])
 
 ; A WordleState (WS) is a (make-wordle [List-of String] String)
@@ -787,19 +685,6 @@
 (define WS-WORD1 (make-wordle (list "CAT") ""))
 (define WS-WORD2 (make-wordle (list "CAT" "DAB") ""))
 (define WS-WORD3 (make-wordle (list "CAT" "DAB" "BAD") ""))
-
-
-; TODO 5/8: Now design the on-key handler by finishing the design for key-wordle.
-;
-;           Notes:
-;           - Make sure you read and understand all the supplied tests, which
-;             capture how the function is supposed to work. Once your code is
-;             complete, all of these tests should pass (and that is sufficient
-;             testing for this function).
-;           - We recommend breaking this function into helpers based upon the
-;             type of supplied key, as you see in the organization of the tests.
-;           - It might help to review Homework 2, where we did a simpler version
-;             of Wordle keyboard handling.
 
 
 ; key-wordle : WS KeyEvent [List-of String] Nat -> WS
@@ -950,18 +835,6 @@
  WS-TYPE3)
 
 
-
-
-; TODO 6/8: And now design the stop-when handler by finishing the design for
-;           end-wordle?.
-;
-;           Notes:
-;           - Make sure you read and understand all the supplied tests, which
-;             capture how the function is supposed to work. Once your code is
-;             complete, all of these tests should pass (and that is sufficient
-;             testing for this function).
-
-
 ; end-wordle? : WS String Nat -> Boolean
 ; determines if the game is over, either because the
 ; (supplied) maximum number of guesses has been made,
@@ -989,19 +862,6 @@
 (check-expect (end-wordle? WS-WORD3 "BAD" 5) #true)
 (check-expect (end-wordle? WS-WORD3 "ETA" 5) #false)
 
-
-
-
-
-; TODO 7/8: Design the function used to draw the final state (when stop-when
-;           returns true) by finishing the design for draw-end-wordle.
-;
-;           Notes:
-;           - Your end screen should indicate...
-;             * Whether the player won or lost
-;             * And if they won, how many guesses it took
-;           - You haven't been supplied tests so that you have some room for
-;             creativity in your visualization :)
 
 
 ; draw-end-wordle : WS String Nat -> Image
@@ -1032,25 +892,6 @@
                (rectangle (* n 75) (* n 75) "solid" "red"))))   
                                      
 
-
-; TODO 8/8: And now, design the to-draw handler by finishing the design for
-;           draw-wordle.
-;
-;           Notes:
-;           - Your play screen should (at least)...
-;             * Visualize all prior guesses (a combination of score, lsp->image,
-;               and row->image are useful here)
-;             * Visualize the current partial guess (partial-guess->image is
-;               quite useful here; note that the length of the correct word
-;               tells you the number of letters per guess)
-;             * Visualize any remaining future guesses (partial-guess->image can
-;               also be quite useful here)
-;           - As a suggestion: separate each piece of the interface into
-;             separate values/functions, then combine via stack/v
-;           - You haven't been supplied tests so that you have some room for
-;             creativity in your interface :)
-
-
 ; draw-wordle : WS String Nat -> Image
 ; visualizes the play state of wordle given the correct answer
 ; and number of available guesses
@@ -1077,9 +918,6 @@
     (stack/v (append (map row->image (score-wordle ws)) (list (draw-current-word ws))
                      (background n s)))))
    
-     
-; When you have completed all the TODOs, it's game time!!! :)
-; Uncomment the function below
 
 
 ; play : String Nat String -> [List-of String]
